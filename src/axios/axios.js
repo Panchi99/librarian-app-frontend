b8a51777a225157e293179cb6b4bbf6cea7d3b67
Panchi99 <Babastrina22@>
2022-04-25 23:17:06 +0200

    Axios base url change!

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newInstance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://librarian-book-app.herokuapp.com/api",
     headers: {
         'Access-Control-Allow-Origin' : '*'
     }
